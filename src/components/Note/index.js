43a245d910186e7c4b20b5ad7d1a3e46025a0ab3
rkgus24 <shrkgus1124@gmail.com>
2023-12-28 01:36:52 +0900

    add : 메인페이지 최종 + 쪽지 초안

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import color from "../../styles/color";
 import font from "../../styles/font";
 
-function Note({ Text1, Text2 }) {
+function Note({Writer, NoteContent, NoteProduct}) {
   return (
     <Box>
-        
+      <TextTitle>{Writer}님의 쪽지</TextTitle>
+      <Product>{NoteProduct}</Product>
+      <Contents>{NoteContent}</Contents>
     </Box>
   );
 }
@@ -18,4 +20,40 @@ const Box = styled.div`
   flex-direction: column;
   align-items: flex-start;
   gap: 16px;
+
+  background: #fff8de;
+  box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.25);
 `;
+
+const TextTitle = styled.span`
+  color: #000;
+
+  font-family: Gmarket Sans;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+const Product = styled.span`
+  color: #000;
+
+  font-family: Gmarket Sans;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+`;
+
+const Contents = styled.span`
+  color: #000;
+
+  text-align: center;
+  font-family: Gmarket Sans;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+`;
+
+export default Note;
--- src/pages/Mail/Collect.js
+++ src/pages/Mail/Collect.js
@@ -2,12 +2,16 @@ import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import { useLocation } from "react-router";
 import { useEffect } from "react";
+import Note from "../../components/Note/index"
 
 function Collect() {
   return (
-    <>
-    <ㅠㅐㅌ
-    </>
+    <Note
+      Writer="2111 원설아"
+      NoteProduct="마루는강쥐 마우스패드"
+      NoteContent="와 진짜 귀여워요!
+1월 9일 종업식 끝나고 사러 가도 될까요?"
+    />
   );
 }
 
